525380e4cbbdc9cb14d06d779f5ed49c701d2753
German Evseev <g3rmanevseev@yandex.ru>
2025-05-27 19:26:06 +0400

    firs comit

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,31 @@
 // @todo: Темплейт карточки
+function createCard(cardData, handleDelete) {
+  const template = document.querySelector("#card-template").content;
+  const cardElement = template.querySelector(".card").cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener("click", () => handleDelete(cardElement));
+
+  return cardElement;
+}
+
+function handleDeleteCard(cardElement) {
+  cardElement.remove();
+}
+
+const placesList = document.querySelector(".places__list");
+
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, handleDeleteCard);
+  placesList.appendChild(card);
+});
 
 // @todo: DOM узлы
 
